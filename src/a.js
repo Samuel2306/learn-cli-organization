6aad1d22261d5479c59787622f9249bd437a3c67
Samuel2306 <1224042766@qq.com>
2018-11-27 20:44:01 +0800

    first commit

---
+++ src/a.js
@@ -0,0 +1,8 @@
+/**
+ * Created by feng.shen on 2018/11/27.
+ */
+export default {
+  a() {
+    console.log('12345');
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by feng.shen on 2018/11/27.
+ */
+import a from './a';
+a.a();
